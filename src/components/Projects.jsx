8fc61b2762ba8180f10ed7c0be1b9e8d7b3ba5a4
Munaf Khan <munafk512@gmail.com>
2025-08-03 17:53:58 +0530

    Updated feature/bugfix/changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,8 +25,8 @@ const Projects = () => {
             <img src={proj5} alt="project-image" className='img-fluid' />
           </div>
           <div className="col-md-6 project-box-txt">
-            <h3 className='text-center text-primary'>The White Chillies – Restaurant Website</h3>
-            <p><i>Tech Stack: React JS, Tailwind CSS, HTML, and CSS</i></p>
+            <h3 className='text-center text-primary mb-3'>The White Chillies – Restaurant Website</h3>
+            <p className='text-center text-danger'><i>Tech Stack: React JS, Tailwind CSS, HTML, and CSS</i></p>
             <p>- Developed a fully responsive restaurant website using React, featuring dynamic pages like Menu, Cart, Checkout, Reservation, and Contact.</p>
             <p>- Integrated cart functionality with real-time updates, order processing, and user interaction through modals and form validation for a seamless online ordering experience.</p>
 
@@ -39,8 +39,8 @@ const Projects = () => {
             <img src={proj4} alt="project-image" className='img-fluid' />
           </div>
           <div className="col-md-6 project-box-txt">
-            <h3 className='text-center text-primary'>MyInfoMate – Multi-API React Web App</h3>
-            <p><i>Tech Stack: React JS, HTML5, CSS3, Bootstrap 5, Axios, and API's</i></p>
+            <h3 className='text-center text-primary mb-3'>MyInfoMate – Multi-API React Web App</h3>
+            <p className='text-center text-danger'><i>Tech Stack: React JS, HTML5, CSS3, Bootstrap 5, Axios, and API's</i></p>
             <p>- MyInfoMate is a responsive React web app that integrates four APIs to offer real-time weather updates, movie details, Indian public holidays, and English word definitions.</p>
             <p>- MyInfoMate is a responsive React web app that integrates four APIs to offer real-time weather updates, movie details, Indian public holidays, and English word definitions.</p>
 
@@ -53,8 +53,8 @@ const Projects = () => {
             <img src={proj3} alt="project-image" className='img-fluid' />
           </div>
           <div className="col-md-6 project-box-txt">
-            <h3 className='text-center text-primary'>Pathshala Education Hub - Free Learning Resources Website</h3>
-            <p><i>Tech Stack: React JS, HTML5, CSS3, Bootstrap 5 and React-Router</i></p>
+            <h3 className='text-center text-primary mb-3'>Pathshala Education Hub - Free Learning Resources Website</h3>
+            <p className='text-center text-danger'><i>Tech Stack: React JS, HTML5, CSS3, Bootstrap 5 and React-Router</i></p>
             <p>Pathshala is a responsive educational hub built with React JS, React-Bootstrap, and Bootstrap 5, offering free study materials and mentor support.</p>
             <p>It features a clean UI with components like a membership form, donation options, and contact page, highlighting form handling, routing, and frontend skills.</p>
 
@@ -67,8 +67,8 @@ const Projects = () => {
             <img src={proj2} alt="project-image" className='img-fluid' />
           </div>
           <div className="col-md-6 project-box-txt">
-            <h3 className='text-center text-primary'>GearUp Auto - Car Website</h3>
-            <p><i>Tech Stack: HTML5, CSS3, Bootstrap 5 and JavaScript</i></p>
+            <h3 className='text-center text-primary mb-3'>GearUp Auto - Car Website</h3>
+            <p className='text-center text-danger'><i>Tech Stack: HTML5, CSS3, Bootstrap 5 and JavaScript</i></p>
             <p>Developed a responsive second-hand car buy-and-sell website using HTML, CSS, JavaScript, and Bootstrap, featuring Home, Buy Car, Sell Car, EMI Calculator, and FAQ pages.</p>
             <p>Emphasized smooth user experience with intuitive navigation and interactive tools for exploring car options and understanding financing.</p>
 
