519c645730be0aec3f1cf8dc25bbba09994d12e9
Munaf Khan <munafk512@gmail.com>
2025-08-23 21:19:25 +0530

    Updated feature/bugfix/changes

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -21,7 +21,7 @@ const Certification = () => {
 
             <div className="container certification-box mb-3">
                 <div className="row mx-auto">
-                    <div className="col-md-6 col-12 mb-3 certification-text">
+                    <div className="col-md-6 mb-5 certification-text">
                         <h4 className='text-primary fs-3'>Complete Frontend Web Development</h4>
                         <ul>
                             <li>Gained hands-on experience in building responsive websites using HTML5, CSS3, and Bootstrap 5.</li>
@@ -29,18 +29,12 @@ const Certification = () => {
                             <li>Developed and practiced modern frontend projects with React, including components, props, and hooks.</li>
                         </ul>
 
+                        <h5 className='mt-3 text-danger'>Issued By - Udemy</h5>
+
                         <Button href={certificate1} className='certification-btn mt-3'>View Certificate</Button>
                     </div>
-                    <div className="col-md-6 mb-3 certification-img text-center">
-                        <img src={cerImg1} alt="certificate-agency" className='img-fluid rounded-circle' />
-                        <h3 className='mt-5 text-danger'>Issued By - Udemy</h3>
-                    </div>
-                </div>
-            </div>
 
-            <div className="container certification-box mb-3">
-                <div className="row mx-auto">
-                    <div className="col-md-6 col-12 mb-3 certification-text">
+                    <div className="col-md-6 mb-5 certification-text">
                         <h4 className='text-primary fs-3'>The Structured Query Language</h4>
                         <ul>
                             <li>Learned to write and optimize SQL queries for data extraction and manipulation.</li>
@@ -48,14 +42,13 @@ const Certification = () => {
                             <li>Gained practical skills in data analysis and reporting through real-world SQL exercises.</li>
                         </ul>
 
+                        <h5 className='mt-3 text-danger'>Issued By - Coursera</h5>
+
                         <Button href={certificate2} className='certification-btn mt-3'>View Certificate</Button>
-                    </div>
-                    <div className="col-md-6 mb-3 certification-img text-center">
-                        <img src={cerImg2} alt="certificate-agency" className='img-fluid rounded-circle' />
-                        <h3 className='mt-5 text-danger'>Issued By - Coursera</h3>
                     </div>
                 </div>
             </div>
+
         </section>
     )
 }
